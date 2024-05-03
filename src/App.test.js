cc23c805094928fedc14cba2104ff74838e7f737
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2024-05-03 16:55:52 +0900

    sample test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('the counter starts at 0', () => {
+  render(<App />); 
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
 });
