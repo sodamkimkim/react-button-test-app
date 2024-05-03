1c1de520058132e4d223abce00e04641d6ef9307
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2024-05-03 17:56:52 +0900

    on/off 버튼 만들기 (toHaveStyle)

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{count}</h3>
-        <div>
           <button data-testid="minus-button" onClick={()=>setCount((prev)=>prev-1)}>-</button>
           <button data-testid="plus-button" onClick={()=>setCount((prev)=>prev+1)}>+</button>
+        <div>
+          <button data-testid="on/off-button" style={{backgroundColor:"blue"}}>on/off</button>
         </div>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,31 +1,37 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
 test('the counter starts at 0', () => {
-  render(<App />); 
-  const counterElement = screen.getByTestId("counter");
-  expect(counterElement).toHaveTextContent(0);
+    render(<App/>);
+    const counterElement = screen.getByTestId("counter");
+    expect(counterElement).toHaveTextContent(0);
 });
 
 test('minus button has correct text', () => {
-  // App 컴포넌트를 렌더링
-  render(<App/>);
-  // screen object를 이용해서 원하는 엘리먼트에 접근 (접근할 때 ID로 접근)
-  const buttonElement= screen.getByTestId("minus-button");
-  // id가 counter인 엘리먼트가 0인지 테스트
-  expect(buttonElement).toHaveTextContent("-");
+    // App 컴포넌트를 렌더링
+    render(<App/>);
+    // screen object를 이용해서 원하는 엘리먼트에 접근 (접근할 때 ID로 접근)
+    const buttonElement = screen.getByTestId("minus-button");
+    // id가 counter인 엘리먼트가 0인지 테스트
+    expect(buttonElement).toHaveTextContent("-");
 });
 
 test('plus button has correct text', () => {
-  render(<App/>);
-  const buttonElement = screen.getByTestId("plus-button");
-  expect(buttonElement).toHaveTextContent("+");
+    render(<App/>);
+    const buttonElement = screen.getByTestId("plus-button");
+    expect(buttonElement).toHaveTextContent("+");
 });
 
 test('when the + button is pressed, the counter changes to 1', () => {
+    render(<App/>);
+    const buttonElement = screen.getByTestId("plus-button");
+    fireEvent.click(buttonElement);
+    const counterElement = screen.getByTestId("counter");
+    expect(counterElement).toHaveTextContent(1);
+});
+
+test('on/off button has blue color', () => {
   render(<App/>);
-  const buttonElement = screen.getByTestId("plus-button");
-  fireEvent.click(buttonElement);
-  const counterElement = screen.getByTestId("counter") ;
-  expect(counterElement).toHaveTextContent(1);
-}) ;
+  const buttonElement = screen.getByTestId("on/off-button");
+  expect(buttonElement).toHaveStyle({backgroundColor:"blue"});
+})
