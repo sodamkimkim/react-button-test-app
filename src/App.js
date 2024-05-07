a4dd7a8a1f3d6c61e3c68454b95fcf8a90d31d88
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2024-05-07 13:29:01 +0900

    on/off 버튼 클릭 시 버튼 disabled

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import {useState} from 'react';
 function App() {
   const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{count}</h3>
-          <button data-testid="minus-button" onClick={()=>setCount((prev)=>prev-1)}>-</button>
-          <button data-testid="plus-button" onClick={()=>setCount((prev)=>prev+1)}>+</button>
+          <button data-testid="minus-button" disabled={disabled} onClick={()=>setCount((prev)=>prev-1)}>-</button>
+          <button data-testid="plus-button" disabled={disabled} onClick={()=>setCount((prev)=>prev+1)}>+</button>
         <div>
-          <button data-testid="on/off-button" style={{backgroundColor:"blue"}}>on/off</button>
+          <button data-testid="on/off-button" style={{backgroundColor:"blue"}} onClick={()=>setDisabled((prev)=>!prev)}>on/off</button>
         </div>
       </header>
     </div>
